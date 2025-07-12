4788d46ccd9fd9a5832cea775917a62eb2c29bf3
Francisco de Assis <chico_fullstack_2023@outlook.com>
2025-07-11 21:24:14 -0300

    update

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,8 +6,8 @@ const tenantRoutes = require('./tenantRoutes');
 const contractRoutes = require('./contractRoutes');
 
 // Agrupa as rotas da aplicação
-router.use('/properties', propertyRoutes);
-router.use('/tenants', tenantRoutes);
-router.use('/contracts', contractRoutes);
+router.use('/api/properties', propertyRoutes);
+router.use('/api/tenants', tenantRoutes);
+router.use('/api/contracts', contractRoutes);
 
 module.exports = router;
